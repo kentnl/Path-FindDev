39ca18a097c57b4dee6ad6584ec784e68f0bf73c
Kent Fredric <kentfredric@gmail.com>
2014-03-04 09:50:56 +1300

    Build results of 8a0c638 (on master)

--- lib/Path/FindDev.pm
+++ lib/Path/FindDev.pm
@@ -4,7 +4,7 @@ use warnings;
 use utf8;
 
 package Path::FindDev;
-$Path::FindDev::VERSION = '0.4.3';
+$Path::FindDev::VERSION = '0.5.0';
 # ABSTRACT: Find a development path somewhere in an upper hierarchy.
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
@@ -130,7 +130,7 @@ Path::FindDev - Find a development path somewhere in an upper hierarchy.
 
 =head1 VERSION
 
-version 0.4.3
+version 0.5.0
 
 =head1 DESCRIPTION
 
--- lib/Path/FindDev/Object.pm
+++ lib/Path/FindDev/Object.pm
@@ -4,7 +4,7 @@ use warnings;
 use utf8;
 
 package Path::FindDev::Object;
-$Path::FindDev::Object::VERSION = '0.4.3';
+$Path::FindDev::Object::VERSION = '0.5.0';
 # ABSTRACT: Object oriented guts to FindDev
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
@@ -262,7 +262,7 @@ Path::FindDev::Object - Object oriented guts to FindDev
 
 =head1 VERSION
 
-version 0.4.3
+version 0.5.0
 
 =head1 SYNOPSIS
 
